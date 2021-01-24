3bd678d6c2419072e818bce9d9586cf38f73607b
Hennan Gadelha <hennanhennan@gmail.com>
2021-01-24 15:25:02 -0300

    DESAFIO [02] Finalizando desafio de calculo distancia

---
+++ Introducao a promação c#/DESAFIO[2] Distancia/calculardistancia/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Distancia 
+{
+    static void Main() 
+    {
+        int kms = Int32.Parse(Console.ReadLine());
+        int minutos = kms * 2;
+        Console.WriteLine(minutos + " minutos");
+    }
+}
