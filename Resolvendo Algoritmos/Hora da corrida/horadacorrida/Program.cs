aca74cb3098ec2746d91861be165711133b6d623
Hennan Gadelha <hennanhennan@gmail.com>
2021-01-24 21:19:57 -0300

    DESAFIO [01] Finalizando desafio hora da corrida

---
+++ Resolvendo Algoritmos/Hora da corrida/horadacorrida/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HoraDaCorrida 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {    
+            string[] vet = Console.ReadLine().Split(" ");
+            
+            int a = int.Parse(vet[0]);
+            int n = int.Parse(vet[1]);
+            
+            int NumeroPlacas = a * n;
+
+            for (int i = 10; i <= 90; i += 10) 
+            {
+                double total = (double)NumeroPlacas / 100 * i;
+                Console.Write(Math.Ceiling(total) + " ");
+            }
+        }
+    }
+}
